ed5ea24caeafc884a55b70c8cf8d319ff95427e8
Jenny <jennyelisa07@aol.com>
2019-07-07 11:14:51 -0400

    js file created, sql file created

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(400) NULL,
+    department_name VARCHAR(400) NULL,
+    price DECIMAL (10,2) NULL,
+    stock_quantity INT(10) NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Cat food", "Pet", 19, 5),
+		("Dog food", "Pet", 15, 5),
+        ("Sunglasses", "Self", 25, 10),
+        ("AirPods", "Entertainment", 200, 6),
+        ("GoT BluRay S2", "Entertainment", 45, 15),
+        ("iPod Classic", "Entertainment", 100, 4),
+        ("Shampoo", "Self", 8, 30),
+        ("Conditoner", "Self", 8, 27),
+        ("Dog Leash", "Pet", 10, 20),
+        ("Cat Nip", "Pet", 2, 50);
+        
